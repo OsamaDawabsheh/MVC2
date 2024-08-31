f00edcaefe5980fcf692212f08eea3e5601a42cc
Osama Dawabsheh <omad10200099@gmail.com>
2024-08-31 22:51:20 +0300

    first commit

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC3.Data;
+using MVC3.Models;
+
+namespace MVC3.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UsersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult NonActive()
+        {
+            var users = _context.users.ToList();
+            var activeUsers = _context.users.Count(users => users.IsActive == true);
+            var nonActiveUsers = _context.users.Where(users => users.IsActive == false).ToList();
+            ViewBag.ActiveUsers = activeUsers;
+            ViewBag.NonActiveUsers = nonActiveUsers.Count();
+            ViewBag.AllUsers = users.Count();
+            return View(nonActiveUsers);
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(User user)
+        {
+            var users = _context.users.Add(user);
+            _context.SaveChanges();
+            return RedirectToAction("Login");
+        }
+
+        public IActionResult Login()
+        {
+
+            return View(); 
+        }
+
+        [HttpPost]
+        public IActionResult Login(User us)
+        {
+            var checkUser = _context.users.Where(user => user.Name == us.Name && user.Password == us.Password);
+            if (checkUser.Any())
+            {
+                return RedirectToAction("NonActive");
+
+            }
+            ViewBag.error = "invalid username or password";
+            return View();
+        }
+
+        public IActionResult Active(int id)
+        {
+            var user = _context.users.Find(id);
+            user.IsActive = true;
+            _context.SaveChanges();
+
+            return RedirectToAction("NonActive");
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC3.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = null!;
+
+        public bool IsActive { get; set; }
+    }
+}
